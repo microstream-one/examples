b087a722db771d5ea3ae90a73399db293f16b94e
hg-ms <h.grunwald@microstream.one>
2019-08-07 08:30:36 +0200

    Eager loading example

---
+++ e006-eagerLoading/src/main/java/one/microstream/sampler/eagerloading/MyRoot.java
@@ -0,0 +1,9 @@
+package one.microstream.sampler.eagerloading;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyRoot 
+{
+	public List<MyData> myObjects = new ArrayList<>();
+}
