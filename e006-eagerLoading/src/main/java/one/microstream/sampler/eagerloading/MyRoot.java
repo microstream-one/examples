d0a36df9dce512f995f294d26bf226c08c75c6e2
hg-ms <h.grunwald@microstream.one>
2019-09-09 09:39:07 +0200

    removed examples naming enumeration renamed example eagerLoading to loading add delete example

--- e006-eagerLoading/src/main/java/one/microstream/sampler/eagerloading/MyRoot.java
+++
@@ -1,9 +0,0 @@
-package one.microstream.sampler.eagerloading;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MyRoot 
-{
-	public List<MyData> myObjects = new ArrayList<>();
-}
---
+++ lazyLoading/src/main/java/one/microstream/sampler/lazyLoading/MyBusinessApp.java
@@ -0,0 +1,20 @@
+
+package one.microstream.sampler.lazyLoading;
+
+import java.util.HashMap;
+
+
+public class MyBusinessApp
+{
+	private final HashMap<Integer, BusinessYear> businessYears = new HashMap<>();
+	
+	public MyBusinessApp()
+	{
+		super();
+	}
+	
+	public HashMap<Integer, BusinessYear> getBusinessYears()
+	{
+		return this.businessYears;
+	}
+}
